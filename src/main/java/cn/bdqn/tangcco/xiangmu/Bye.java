2c7878127ce93ef38373b4c8b93f6ce33616b67c
shangdi1122 <1399070052@qq.com>
2017-08-04 12:27:49 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/xiangmu/Bye.java
@@ -0,0 +1,19 @@
+package cn.bdqn.tangcco.xiangmu;
+
+import org.junit.Test;
+
+/**
+ * Created by THINKPAD on 2017/8/4.
+ */
+public class Bye {
+
+    @Test
+    public void sayGoodBye() {
+
+
+        System.out.println("好");
+        System.out.println("好");
+        System.out.println("做");
+        System.out.println("人");
+    }
+}
---
+++ src/main/java/cn/bdqn/tangcco/xiangmu/Thanks.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.xiangmu;
+
+/**
+ * Created by THINKPAD on 2017/8/4.
+ */
+public class Thanks {
+
+    public static void main(String[] args) {
+        System.out.println("孙子你别跑！！！");
+
+
+        System.out.println("孙子，有种别追！！！！");
+    }
+}
